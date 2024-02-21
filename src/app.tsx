3c8c79d2c004fcaf37182c4befc8d2dc9f5b51d0
Marcos <marcosallanm1to@gmail.com>
2024-02-21 14:31:27 -0300

    feat: show fetching loader and move filters to form

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { FileDown, Filter, MoreHorizontal, Plus, Search } from "lucide-react"
+import { Plus, Search, Filter, FileDown, MoreHorizontal, Loader2 } from "lucide-react"
 import { Header } from "./components/header"
 import { Tabs } from "./components/tabs"
 import { Button } from "./components/ui/button"
@@ -7,7 +7,7 @@ import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from ".
 import { Pagination } from "./components/pagination"
 import { keepPreviousData, useQuery } from '@tanstack/react-query'
 import { useSearchParams } from "react-router-dom"
-import { useState } from "react"
+import { FormEvent, useState } from "react"
 
 export interface TagResponse {
   first: number
@@ -33,7 +33,7 @@ export function App() {
 
   const page = searchParams.get('page') ? Number(searchParams.get('page')) : 1
 
-  const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
+  const { data: tagsResponse, isLoading, isFetching } = useQuery<TagResponse>({
     queryKey: ['get-tags', urlFilter, page],
     queryFn: async () => {
       const response = await fetch(`http://localhost:3333/tags?_page=${page}&_per_page=10&title=${urlFilter}`)
@@ -44,7 +44,9 @@ export function App() {
     placeholderData: keepPreviousData,
   })
 
-  function handleFilter() {
+  function handleFilter(event: FormEvent) {
+    event.preventDefault()
+    
     setSearchParams(params => {
       params.set('page', '1')
       params.set('filter', filter)
@@ -70,10 +72,11 @@ export function App() {
             <Plus className="size-3" />
             Create new
           </Button>
+          {isFetching && <Loader2 className="size-4 animate-spin text-zinc-500" />}
         </div>
 
         <div className="flex items-center justify-between">
-          <div className="flex items-center">
+          <form onSubmit={handleFilter} className="flex items-center gap-2">
             <Input variant='filter'>
               <Search className="size-3" />
               <Control
@@ -82,11 +85,11 @@ export function App() {
                 value={filter}
               />
             </Input>
-            <Button onClick={handleFilter}>
+            <Button type="submit">
                <Filter className="size-3" />
-               Filter
+               Apply filter
             </Button>
-          </div>
+          </form>
 
           <Button>
             <FileDown className="size-3" />
